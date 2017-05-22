5b7dd94c9fd78fbd1a297a5b405e8d66f32fe2a3
David <david.useche@mail.escuelaing.edu.co>
2017-05-22 07:25:55 -0500

    Cambios despliegue

--- src/main/java/edu/eci/is/registro/RegistroApplication.java
+++ src/main/java/edu/eci/is/registro/RegistroApplication.java
@@ -3,14 +3,22 @@ package edu.eci.is.registro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("edu.eci.is.registro.entities")
 @EnableJpaRepositories("edu.eci.is.registro.repositories")
 @SpringBootApplication
-public class RegistroApplication {
+public class RegistroApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistroApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(RegistroApplication.class, args);
 	}
+
 }
